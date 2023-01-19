b769c99063b7f0a727e8161beea111635a45a950
obryanc23 <119616236+obryanc23@users.noreply.github.com>
2023-01-19 11:23:25 -0500

    background and stars now

--- main.ts
+++ main.ts
@@ -1,1 +1,48 @@
-
+function backgroundStars () {
+    mySprite = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . 1 . . . . . . 
+        . . . . . . . . 1 . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Player)
+    mySprite = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . 1 . . . . . . . . 
+        . . . . . . 1 1 . . . . . . . . 
+        . . . . . . . 1 . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Player)
+    mySprite.setVelocity(0, randint(10, 50))
+    mySprite.setPosition(randint(0, 160), 0)
+}
+function backgrounndColor () {
+    scene.setBackgroundColor(15)
+}
+let mySprite: Sprite = null
+backgrounndColor()
+game.onUpdateInterval(randint(150, 250), function () {
+    backgroundStars()
+})
